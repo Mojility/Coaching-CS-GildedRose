1ccd77675853bec7872a5adcd3da039ca4952e88
Stacey Vetzal <stacey@vetzal.com>
2017-11-27 11:55:37 -0500

    adding characterizer project

---
+++ Characterizer/characterize/InterestingData.cs
@@ -0,0 +1,16 @@
+﻿namespace Characterizer.characterize
+{
+    class InterestingData
+    {
+        public string[] Names = new string[]
+        {
+            "Normal Item",
+            "Aged Brie",
+            "Backstage passes to a TAFKAL80ETC concert",
+            "Sulfuras, Hand of Ragnaros"
+        };
+
+        public int[] QualityBoundaries = new int[] { 0, 50 };
+        public int[] SellInBoundaries = new int[] { 0, 6 };
+    }
+}
